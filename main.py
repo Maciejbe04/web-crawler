da3f99731e80c5bdf49fd990c8f9df639632cba5
MaciejBe04 <s31129@pjwstk.edu.pl>
2026-09-25 20:07:53 +0200

    changed algorithm to bfs, need to fix performance issues caused by wrong thread management

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from concurrent.futures import ThreadPoolExecutor, wait
 import sys, getopt,os
 from rich import print
 import signal
-
+import queue
 
 class Spider:
 
@@ -17,12 +17,10 @@ class Spider:
 
     extensions = ['.js', '.html', ".php", '.css', '.ico', "txt"]
 
-    depth_dict = {}
-
     visited = set()
+    found = set()
     static_page_pattern = r"(https?:\/\/[\w][\w\W]+?[\"\'\>\<\s])"
     referer_pattern = r"href=\"(.+?)[\"\'\>\<\s]"
-    current_depth = 0
 
     def __init__(self, url:str, depth:int, threads:int):
         self.url = url
@@ -31,129 +29,132 @@ class Spider:
         Spider.scope = re.search(Spider.scope_regex, self.url).group(1)
         self.lock = threading.Lock()
         self.threads = threads
-        self.tasks = []
-        Spider.executor = None
+        self.queue = queue.Queue()
+        self.depth_dict = {}
+        self.current_depth = 0
 
-    def init_crawl(self, url):
+
+
+    def grab_links(self, url):
 
         content = ""
-
-        response_url = self.parse_url(url)
-
-        if response_url in Spider.visited:
-            return    
         
-        try:
-            content = requests.get(self.url.strip()).text
-            Spider.visited.add(self.url)
-            response_url = self.url
-
-        except requests.exceptions.ConnectionError as ex:
-            print("couldnt connect")
-            return 
-
-        Spider.visited.add(response_url)
-
-        list_of_static_links = re.findall(Spider.static_page_pattern, content)
-        list_of_referals = re.findall(Spider.referer_pattern, content)
-
-        all_links = self.filter_scope(list_of_static_links) + list_of_referals
-
-        with ThreadPoolExecutor(max_workers=self.threads) as ex:
-
-            self.executor = ex
-
-            for link in all_links:
-
-                normalized_path = self.normalize_path(response_url, link)
-                
-                if "https://" in link or "http://" in link:
-                    normalized_path = link
-                
-                if normalized_path in Spider.visited:
-                    continue
-
-                future = ex.submit(self.crawl, normalized_path, 0, ex)
-                self.tasks.append(future)
-
-            while True:
-                with self.lock:
-                    pending = [f for f in self.tasks if not f.done()]
-                    if not pending:
-                        break
-                wait(pending)
-                
-                                   
-            
-  
-
-    def crawl(self,url, depth,executor):
-
-        if depth > self.depth:
-            return 
-
-        content = ""
-
         response_url = self.parse_url(url)
+
+        print(f"{response_url} parsed" )
 
         with self.lock:
             if response_url in Spider.visited:
-                return 
+                return [], None
             Spider.visited.add(response_url)
-
-        print(depth)
-        
+    
         try:
-            response = requests.get(response_url, timeout=5)
-
+            
+            response = requests.get(response_url)
+            response_url = response.url
             content = response.text
 
-            response_url = response.url
-                
+            print(f"{response_url} response" )
+
             if response.status_code not in Spider.valid_codes:
-                return 
-
+                Spider.visited.remove(response_url)
+                return [], None
+                
         except requests.exceptions.ConnectionError as ex:
-            print(f"Couldnt connect to adress {response_url}")
-            return 
-
+            print("couldnt connect")
+            return [], None
 
         list_of_static_links = re.findall(Spider.static_page_pattern, content)
         list_of_referals = re.findall(Spider.referer_pattern, content)
 
         all_links = self.filter_scope(list_of_static_links) + list_of_referals
 
-        for link in all_links:
+        return all_links, response_url
+
+    def bfs(self, url):
+
+        links, response_url = self.grab_links(url)
+
+        if response_url == None:
+            self.queue.task_done()
+            return 
+
+        for link in links:
 
             normalized_path = self.normalize_path(response_url, link)
-
+            
             if "https://" in link or "http://" in link:
                 normalized_path = link
 
-            if normalized_path not in Spider.visited:
-                f = executor.submit(self.crawl, normalized_path, depth+1, executor)
+            with self.lock:
+                if normalized_path in Spider.found:
+                    continue
+                self.depth_dict[normalized_path] = self.current_depth
+                Spider.found.add(normalized_path)
+
+            if self.current_depth == self.depth:
                 with self.lock:
-                    self.tasks.append(f)
-            else:
+                    Spider.found.add(normalized_path)
                 continue
+
+            self.queue.put(normalized_path)
+
+        self.queue.task_done()
+                     
+    def queue_handler(self):
+
+        self.queue.put(self.url)
+        self.depth_dict[self.url] = 0
+
+
+        with ThreadPoolExecutor(max_workers=self.threads) as exe:
+
+            while True:
+                self.current_depth += 1
+                if self.current_depth > self.depth:
+                    break
+
+                while True:
+                    futures = []
+                    for _ in range(self.threads):
+                        try:
+                            link = self.queue.get(block=False)
+                        except queue.Empty:
+                            break
+
+                        future = exe.submit(self.bfs, link)
+
+                        futures.append(future)
+                        
+                    if len(futures) == 0:
+                        return False
+
+                    if self.current_depth != self.depth:
+                        wait(futures)
+            
+   
+    
 
     def initialize(self):
 
         print(f"[*] Crawling... [bold red]{self.url}[/bold red]")
         print(f"[*] Options THREADS = [bold green]{self.threads}[/bold green], DEPTH = [bold bright_magenta]{self.depth}[/bold bright_magenta]")
 
-        self.init_crawl(self.url)
+        self.queue_handler()
 
     def format_results(self):
 
         print(f"[+] Found links for [bold red]{self.url}[/bold red] website")
         print("=======================================")
 
-        for link in self.visited:
+        for link in Spider.found:
             print(f"[+] [bold green]{link}[/bold green]")
         print("=======================================")
-        print(f"[+] Found [bold blink medium_spring_green]{len(self.visited)} [/bold blink medium_spring_green]links")
+        print(f"[+] Found [bold blink medium_spring_green]{len(Spider.found)} [/bold blink medium_spring_green]links")
         print("=======================================")
+        print(self.depth_dict)
+        print(len(self.depth_dict))
+        print(f"Working links {len(Spider.visited)}")
 
 
     def validate_fields(self):
@@ -197,17 +198,6 @@ def usage():
     print("-h --help view help")
 
 
-def handle_signal(signum,frame):
-
-    if Spider.executor is not None:
-        Spider.executor.shutdown(wait=True, cancel_futures=True)
-        with open('output.txt', 'w') as file:
-            for link in Spider.visited:
-                file.write(link + "\n")
-
-    sys.exit(2)
-
-
 def main():
 
 
@@ -243,10 +233,10 @@ def main():
             usage()
             exit(2)
 
+
     spider = Spider(url=url, depth=depth, threads=threads)
 
     start_time = time.time()
-    signal.signal(signal.SIGINT, handle_signal)
     spider.initialize()
     stop_time = time.time()
     spider.format_results()
@@ -256,3 +246,4 @@ def main():
 if __name__ == "__main__":
     os.environ.pop("NO_COLOR", None)
     main()
+ 
